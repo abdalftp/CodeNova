7dd4a5261f8e2f9f0a5ce115a8baf5cdc71f8ba7
Abdal <Abdal@DESKTOP-CB4E75Q>
2021-08-17 19:19:20 +0530

    commit changes

---
+++ Projrct/projectAPI/Controllers/ProjectsController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Project.Entities;
+using projectAPI.DAL;
+
+namespace projectAPI.Controllers
+{
+    public class ProjectsController : ApiController
+    {
+        private PContext db = new PContext();
+        
+        // GET: api/Projects
+        public IQueryable<Project.Entities.Project> GetProject()
+        {
+            
+            return db.Project;
+        }
+
+        // GET: api/Projects/5
+        [ResponseType(typeof(Project.Entities.Project))]
+        public IHttpActionResult GetProject(int id)
+        {
+            Project.Entities.Project project = db.Project.Find(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(project);
+        }
+        
+        // PUT: api/Projects/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProject(int id, Project.Entities.Project project)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != project.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(project).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Projects
+        [ResponseType(typeof(Project.Entities.Project))]
+        public IHttpActionResult PostProject(Project.Entities.Project project)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Project.Add(project);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = project.ID }, project);
+        }
+
+        // DELETE: api/Projects/5
+        [ResponseType(typeof(Project.Entities.Project))]
+        public IHttpActionResult DeleteProject(int id)
+        {
+            Project.Entities.Project project = db.Project.Find(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            db.Project.Remove(project);
+            db.SaveChanges();
+
+            return Ok(project);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProjectExists(int id)
+        {
+            return db.Project.Count(e => e.ID == id) > 0;
+        }
+    }
+}
